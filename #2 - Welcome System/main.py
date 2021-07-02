b3e51cb1c62faa996724bc24c8095dbb929aa0e8
CapichePasta <85986723+CapichePasta@users.noreply.github.com>
2021-07-02 14:51:24 +0530

    Update main.py

--- #2 - Welcome System/main.py
+++ #2 - Welcome System/main.py
@@ -16,11 +16,11 @@ client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event 
 async def on_member_join(member):
-    if member.guild.name == "Pasta Sempa":
+    if member.guild.name == "Server_Name":
         embed = discord.Embed(description=f'Hey {member.mention}, Welcome to **{member.guild.name}**',
                                 colour=discord.Colour.red())
         await client.get_channel(854624186118438933).send(embed=embed)
-        role = discord.utils.get(member.guild.roles, name='Member')
+        role = discord.utils.get(member.guild.roles, name='ROLE_NAME')
         await member.add_roles(role)
 
 @client.event
